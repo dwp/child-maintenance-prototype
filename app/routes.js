34c2ea508f406588307a0c7d8c039c7454cd3529
ben raine <ben.raine.digitaldwp@gmail.com>
2018-08-29 14:14:09 +0100

    dynamic journey

--- app/routes.js
+++ app/routes.js
@@ -6,5 +6,7 @@ const router = express.Router()
 const alphaPrototypeV1Routes = require('./routes/alpha-prototype-1');
 router.use('/alpha-prototype-1', alphaPrototypeV1Routes)
 
+const alphaPrototypeV2Routes = require('./routes/alpha-prototype-2');
+router.use('/alpha-prototype-2', alphaPrototypeV2Routes)
 
 module.exports = router
--- app/routes/alpha-prototype-2.js
+++ app/routes/alpha-prototype-2.js
@@ -6,7 +6,7 @@ const router = express.Router()
 // UK resident
 // If either parent isn't a habitual resident of the UK, this directs them to the FBA information. 
 router.get('/are-both-parents-habitually-resident-in-the-uk', (req, res) => {
-    res.render('alpha-prototype-1/questions/habitually-resident.njk')
+    res.render('alpha-prototype-2/questions/habitually-resident.njk')
 })
 
 router.post('/are-both-parents-habitually-resident-in-the-uk', function (req, res) {
@@ -24,42 +24,78 @@ router.post('/are-both-parents-habitually-resident-in-the-uk', function (req, re
 
 // court order
 router.get('/is-there-a-court-order-for-child-maintenance-in-place', (req, res) => {
-    res.render('alpha-prototype-1/questions/court-order.njk')
+    res.render('alpha-prototype-2/questions/court-order.njk')
 })
 
 router.post('/is-there-a-court-order-for-child-maintenance-in-place', (req, res) => {
+    let courtOrder = req.session.data['court-order']
+    if (courtOrder ==="no") {
+
     res.redirect('is-child-under-20')
+    }
+    else {
+        res.redirect("static-pages/courts")
+    }
 })
 
 // child age
 router.get('/is-child-under-20', (req, res) => {
-    res.render('alpha-prototype-1/questions/child-age.njk')
+    res.render('alpha-prototype-2/questions/child-age.njk')
 })
 
 router.post('/is-child-under-20', (req, res) => {
-    res.redirect('what-information-do-you-have-about-the-other-parent')
+    let childAge = req.session.data['child-age']
+    if (childAge ==="no") {
+        res.redirect('-------')
+    }
+    else {
+        res.redirect('what-information-do-you-have-about-the-other-parent')
+        }
 })
 
 // parent info
 router.get('/what-information-do-you-have-about-the-other-parent', (req, res) => {
-    res.render('alpha-prototype-1/questions/other-parent.njk')
+    res.render('alpha-prototype-2/questions/other-parent.njk')
 })
 
 router.post('/what-information-do-you-have-about-the-other-parent', (req, res) => {
-    res.redirect('domestic-abuse')
+    res.redirect('in-contact')
 })
+
+// in contact with other parent
+router.get('/in-contact', (req, res) => {
+    res.render('alpha-prototype-2/questions/in-contact.njk')
+})
+
+router.post('/in-contact', (req, res) => {
+    let inContact = req.session.data['in-contact']
+    if (inContact ==='no') {
+        res.redirect('static-pages/what-cms-can-do')
+    }
+    else {
+        res.redirect('domestic-abuse')
+    }
+
+
+})
+
+
+
 
 // domestic abuse
 router.get('/domestic-abuse', (req, res) => {
-    res.render('alpha-prototype-1/questions/abuse.njk')
+    res.render('alpha-prototype-2/questions/abuse.njk')
 })
 
 router.post('/domestic-abuse', (req, res) => {
-    res.redirect('fba')
+    let domesticAbuse = req.session.data['domestic-abuse']
+    if (domesticAbuse === 'no') {
+    res.redirect('fba') }
+    else { res.redirect('static-pages/abuse-fba')}
 })
 // fba
 router.get('/fba', (req, res) => {
-    res.render('alpha-prototype-1/questions/fba.html')
+    res.render('alpha-prototype-2/questions/fba.html')
 })
 
 module.exports = router
