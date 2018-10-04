6b584fc49cfce71698211dbc8ec41b8f1deae3b2
ben raine <ben.raine.digitaldwp@gmail.com>
2018-10-04 15:12:04 +0100

    knockout q's

--- app/routes/alpha-prototype-5.js
+++ app/routes/alpha-prototype-5.js
@@ -40,4 +40,52 @@ router.post('/conversation', function (req, res) {
         res.redirect('no-communicate')
 })
 
+router.get('/are-both-parents-habitually-resident-in-the-uk', (req, res) => {
+    res.render('alpha-prototype-4/habitually-resident.njk')
+})
+
+router.post('/are-both-parents-habitually-resident-in-the-uk', function (req, res) {
+
+    let habituallyResident = req.session.data['habitually-resident']
+    if (habituallyResident === "no") {
+        res.redirect('not-cms-eligible')
+    } else {
+        res.redirect('is-there-a-court-order-for-child-maintenance-in-place')
+    }
+})
+
+
+
+
+// court order
+router.get('/is-there-a-court-order-for-child-maintenance-in-place', (req, res) => {
+    res.render('alpha-prototype-4/court-order.njk')
+})
+
+router.post('/is-there-a-court-order-for-child-maintenance-in-place', (req, res) => {
+    let courtOrder = req.session.data['court-order']
+    if (courtOrder === "no") {
+
+        res.redirect('is-child-under-20')
+    }
+    else {
+        res.redirect("not-cms-eligible")
+    }
+})
+
+// child age
+router.get('/is-child-under-20', (req, res) => {
+    res.render('alpha-prototype-4/child-age.njk')
+})
+
+router.post('/is-child-under-20', (req, res) => {
+    let childAge = req.session.data['child-age']
+    if (childAge === "no") {
+        res.redirect('not-cms-eligible')
+    }
+    else {
+        res.redirect('phone')
+    }
+})
+
 module.exports = router
