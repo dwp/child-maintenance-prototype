b261a6cd3497ceb95bd369212be97e211288a9a2
ben raine <ben.raine.digitaldwp@gmail.com>
2018-11-06 13:55:23 +0000

    v5

--- app/routes/v5.js
+++ app/routes/v5.js
@@ -16,4 +16,58 @@ router.post('/paying-receiving', (req, res) => {
     }
 })
 
+
+router.get('/do-you-receive-benefits-r', (req, res) => {
+    res.render('v5/benefits-q-r')
+})
+
+router.post('/do-you-receive-benefits-r', (req, res) => {
+    let receiveBenefits2 = req.session.data['do-you-receive-benefits-r']
+    if (receiveBenefits2 === "yes") {
+        res.redirect('benefits-r')
+    }
+    else {
+        res.redirect('wages-r')
+    }
+})
+
+
+router.get('/do-you-receive-benefits-p', (req, res) => {
+    res.render('v5/benefits-q-p')
+})
+
+router.post('/do-you-receive-benefits-p', (req, res) => {
+    let receiveBenefits = req.session.data['do-you-receive-benefits-p']
+    if (receiveBenefits === "yes") {
+        res.redirect('benefits-p')
+    }
+    else {
+        res.redirect('wages-p')
+    }
+})
+
+
+router.get('/child-age', (req, res) => {
+    res.render('v5/cost-tool')
+})
+
+router.post('/child-age', (req, res) => {
+    let childAge = req.session.data['child-age']
+    if (childAge == "baby") {
+
+        res.redirect('baby-food.html')
+    }
+    else if (childAge == "toddler")
+    {
+        res.redirect('toddler-food.html')
+    }
+    else if (childAge == "child")
+    {
+        res.redirect('child-food.html')
+    }
+    else {
+        res.redirect('teenager-food.html')
+}
+})
+
 module.exports = router
