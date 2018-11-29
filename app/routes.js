09e634fe7c4125b9facd48929324af69bb81f23b
ben raine <ben.raine.digitaldwp@gmail.com>
2018-11-29 15:57:24 +0000

    6.1

--- app/routes.js
+++ app/routes.js
@@ -32,6 +32,10 @@ router.use('/v5', v5Routes)
 const v6Routes = require('./routes/v6');
 router.use('/v6', v6Routes)
 
+
+const v61Routes = require('./routes/v6.1');
+router.use('/v6.1', v61Routes)
+
 const calc2Routes = require('./routes/calc2');
 router.use('/calc2', calc2Routes)
 
