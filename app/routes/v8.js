860cc95f183d9aed8a1b88b4f8724ceec8bb5339
ben raine <ben.raine.digitaldwp@gmail.com>
2018-12-07 12:31:57 +0000

    eligibility

--- app/routes/v8.js
+++ app/routes/v8.js
@@ -47,5 +47,54 @@ router.post('/do-you-receive-benefits-p', (req, res) => {
 })
 
 
+router.get('/are-both-parents-habitually-resident-in-the-uk', (req, res) => {
+    res.render('v8/habitually-resident.njk')
+})
+
+router.post('/are-both-parents-habitually-resident-in-the-uk', function (req, res) {
+
+    let habituallyResident = req.session.data['habitually-resident']
+    if (habituallyResident === "no") {
+        res.redirect('fba')
+    } else {
+        res.redirect('is-there-a-court-order-for-child-maintenance-in-place')
+    }
+})
+
+
+
+
+// court order
+router.get('/is-there-a-court-order-for-child-maintenance-in-place', (req, res) => {
+    res.render('v8/court-order.njk')
+})
+
+router.post('/is-there-a-court-order-for-child-maintenance-in-place', (req, res) => {
+    let courtOrder = req.session.data['court-order']
+    if (courtOrder === "no") {
+
+        res.redirect('is-child-under-20')
+    }
+    else {
+        res.redirect("fba")
+    }
+})
+
+// child age
+router.get('/is-child-under-20', (req, res) => {
+    res.render('v8/child-age.njk')
+})
+
+router.post('/is-child-under-20', (req, res) => {
+    let childAge = req.session.data['child-age']
+    if (childAge === "no") {
+        res.redirect('fba')
+    }
+    else {
+        res.redirect('apply-cms')
+    }
+})
+
+
 
 module.exports = router
