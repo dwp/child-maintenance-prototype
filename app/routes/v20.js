2bde62642ff2ea8ec66bcdfc61129eeed69899cd
James Ashcroft <james.ashcroft@engineering.digital.dwp.gov.uk>
2019-04-29 11:53:21 +0100

    more routes fixes

--- app/routes/v20.js
+++ app/routes/v20.js
@@ -8,7 +8,7 @@ router.get('/paying-receiving', (req, res) => {
 router.post('/paying-receiving', (req, res) => {
     let payingReceiving = req.session.data['paying-receiving']
     if (payingReceiving === "paying") {
-        res.redirect('paying-multiple')
+        res.redirect('payment-to-one-or-more-parents')
     }
     else {
         res.redirect('receiving-multiple')
@@ -16,6 +16,20 @@ router.post('/paying-receiving', (req, res) => {
 })
 router.get('/paying-multiple', (req, res) => {
     res.render('test/paying-multiple')
+})
+
+router.get('/paying-receiving', (req, res) => {
+    res.render('test/paying-receiving')
+})
+////////////////////////////////////////////////////////////////////////
+router.post('/paying-multiple-parents', (req, res) => {
+    let payingMultiple = req.session.data['paying-multiple-parent']
+    if (payingMultiple === "yes") {
+        res.redirect('paying-multiple-parents')
+    }
+    else {
+        res.redirect('')
+    }
 })
 
 module.exports = router
